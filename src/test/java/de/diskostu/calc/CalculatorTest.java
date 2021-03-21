40e25894619370f799275188d90ed817c8f1c471
diskostu <denny.kluge@gmail.com>
2021-03-21 21:03:16 +0100

    added log4j2 support

--- src/test/java/de/diskostu/calc/CalculatorTest.java
+++ src/test/java/de/diskostu/calc/CalculatorTest.java
@@ -1,11 +1,13 @@
 package de.diskostu.calc;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
  * Tests for {@link Calculator}.
  */
+@Log4j2
 public class CalculatorTest {
 
     @Test
